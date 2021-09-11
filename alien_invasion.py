42d254cd3eaa332f5af3a179bad3f24313e45467
Andrey Plekhanov <ierehon91@gmail.com>
2021-09-11 16:42:46 +0300

    Создан класс Setting для хранения всех настроек игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,19 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и повидением игры"""
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")  # Название окна
-        self.bg_color = (230, 230, 230)  # Цвет фона
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -20,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего игрового экрана
             pygame.display.flip()
