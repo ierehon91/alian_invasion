12cb8d430d1a83e9e5764324fdba97045086ca36
Andrey Plekhanov <ierehon91@gmail.com>
2021-09-11 16:24:52 +0300

    Создан класс AlienInvasion

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и повидением игры"""
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            # Отслеживание событий клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Отображение последнего игрового экрана
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
