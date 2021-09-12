ceef862b3d78a8fe5bb44f0336a183de6d9a0028
Andrey Plekhanov <ierehon91@gmail.com>
2021-09-12 13:37:58 +0300

    Рефакторинг метода run_game на 2 подметода отрабатывающий события и и обновления изображения на экране. Заменен файл изображения коробля на более маленький

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,16 +21,24 @@ class AlienInvasion:
         """Запуск основного цикла игры"""
         while True:
             # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._update_screen()
 
-            # Отображение последнего игрового экрана
-            pygame.display.flip()
+    def _check_events(self):
+        """Отробатывает нажатие клавиш и движение мышки"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет содержимое на экране и отображает новый экран"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Отображение последнего игрового экрана
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
