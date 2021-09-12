73de61567359b07039a59d2f8bdfb51eb2d2549c
Andrey Plekhanov <ierehon91@gmail.com>
2021-09-12 10:46:58 +0300

    Добавлены изображение и  класс коробля, заменен фон экрана под цвет фона для изображения коробля

---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Ship():
+    """Класс для управления кораблем"""
+    def __init__(self, ai_game):
+        """Инициализируем корабль и задаем его начальную позицию"""
+        self.screen = ai_game.screem
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружаем изображение коробля и получаем прямоугольник
+        self.image = pygame.image.load('images\\ship.bmp')
+        self.rect = self.image.get_rect()
+        # Каждый новый корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Рисуем корабль в текущей позиции"""
+        self.screen.blit(self.image, self.rect)
