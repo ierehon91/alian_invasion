9bb462fc9ebe5174c78b2cffe9e8489872da5f5b
Andrey Plekhanov <ierehon91@gmail.com>
2021-09-14 09:21:41 +0300

    Релизовано перемещение влево при нажатии на кнопку влево

--- ship.py
+++ ship.py
@@ -10,6 +10,7 @@ class Ship():
 
         # Флаг перемещения
         self.moving_right = False
+        self.moving_left = False
 
         # Загружаем изображение коробля и получаем прямоугольник
         self.image = pygame.image.load('images\\ship.bmp')
@@ -21,6 +22,8 @@ class Ship():
         """Обновляет позицию коробля с учетом флага"""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Рисуем корабль в текущей позиции"""
